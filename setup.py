6412d787c0bf7c4e59b6ef08bc255eada7502211
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-07 09:34:09 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cache',
+    version='1.0.0',
+    author='Ryan Kroon',
+    author_email='rykroon.tech@gmail.com',
+    packages=find_packages(),
+)
